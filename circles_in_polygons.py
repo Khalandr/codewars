41ff973d4a9d513d865cddfe7ab8c5f361f4db78
Andrii <khaliavkinandrii@gmail.com>
2024-01-10 22:48:27 +0100

    polygon square

--- circles_in_polygons.py
+++ circles_in_polygons.py
@@ -1,3 +1,15 @@
-def circle_diameter(sides, side_length): 
-    # your code here
-    pass
+import math
+
+# def circle_diameter(sides, side_length): 
+    
+
+def polygon_square(sides, side_length):
+
+    pols_sq = math.sqrt(((sides*side_length**2)/4)*(1/math.tan(180/sides)))
+    print(pols_sq)
+    return pols_sq
+
+polygon_square(3, 4)
+    
+
+
